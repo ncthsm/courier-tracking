6e4e4f9b1bf2e0061fd6c467546378c53346f495
Necati Haşim <necatihasim@gmail.com>
2024-12-06 16:10:00 +0300

    DistanceCalculationFactory Error handling added.

--- src/main/java/com/migros/couriertracking/service/factory/DistanceCalculationFactory.java
+++ src/main/java/com/migros/couriertracking/service/factory/DistanceCalculationFactory.java
@@ -6,7 +6,6 @@ import com.migros.couriertracking.service.strategy.DistanceCalculationStrategy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
